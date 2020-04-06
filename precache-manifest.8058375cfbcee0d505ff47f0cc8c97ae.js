835d145deeddf3bc3594074f23b367308569dcf4
Mika <mikagurrola90@gmail.com>
2020-04-06 13:25:13 -0500

    Updates

---
+++ precache-manifest.8058375cfbcee0d505ff47f0cc8c97ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74548cc640d8d67bab972a5ce1de0027",
+    "url": "/react-countries-api/index.html"
+  },
+  {
+    "revision": "cfe133e8668134ab35e3",
+    "url": "/react-countries-api/static/css/main.a90b2569.chunk.css"
+  },
+  {
+    "revision": "92a59c78e84813aa598a",
+    "url": "/react-countries-api/static/js/2.1dfed9b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-countries-api/static/js/2.1dfed9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe133e8668134ab35e3",
+    "url": "/react-countries-api/static/js/main.395f1c11.chunk.js"
+  },
+  {
+    "revision": "1b59b8024db518010168",
+    "url": "/react-countries-api/static/js/runtime-main.75662447.js"
+  }
+]);
